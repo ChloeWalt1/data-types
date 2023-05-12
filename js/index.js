cff56f1def7115f8ac8c1ae7d8a82ad086f22309
ChloeWalt1 <125471864+ChloeWalt1@users.noreply.github.com>
2023-05-12 16:30:40 +0200

    data-types
    week 2 js exercise

---
+++ js/index.js
@@ -0,0 +1,28 @@
+var name = "Chloe"
+var surname = "Walt"
+
+var age = 19
+
+const subject = "Development"
+
+console.log("Hello, my name is " + name +  " " + surname + ", "+"I am " + age + " years old and I am studying " + subject )
+
+let numOne = 2
+let numTwo = 10
+let numThree = 14
+let numFour = 4
+
+let answer = 0
+
+console.log(numOne + numTwo/numThree * numFour)
+
+var timmy = 10
+var susan = 25
+
+var timmyTax = timmy*0.15
+var susanTax = susan*0.26
+
+var value =  timmyTax + susanTax
+
+console.log("The tax man will claim from susan and timmy combined, R" + value)
+
